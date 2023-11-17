03f53d5be3871c6f97f2e758c8d33eef8b671dec
Akumar <128833380+Adityaakr@users.noreply.github.com>
2023-11-17 23:13:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-vite-app/', // Update to match your Vercel deployment path
+  base: '/', // Update to match your Vercel deployment path
   build: {
     outDir: 'dist',
   },
