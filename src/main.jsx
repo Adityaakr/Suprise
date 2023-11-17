2dd4bf3b0523ea4fd64eb6e7717e936b51b54abb
Akumar <128833380+Adityaakr@users.noreply.github.com>
2023-11-17 23:38:22 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
  <Navbar/>
  <Intro/>
  <CardOne username='Beautiful Portrait' btnText='Shows Beautiful Indian Attire and representing indian culture' 
- image="/home/aditya/Suprise/public/didi.png"/>
+ image="/Suprise/public/didi.png"/>
  <CardOne username='Didi & JIJU' btnText='Didi and Jiju, a perfect pair, complementing each other with love and laughter, creating a forever bond.' 
  image="/public/didi.png"/>
  <CardOne username='Goat Virat Kohli' btnText='One and only Virat Kohli, Legend.' 
