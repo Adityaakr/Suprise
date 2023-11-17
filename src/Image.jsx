cfe1b4060b64a5334404ed17605e302040f1adab
Akumar <128833380+Adityaakr@users.noreply.github.com>
2023-11-17 23:30:55 +0530

    Update Image.jsx

--- src/Image.jsx
+++ src/Image.jsx
@@ -4,9 +4,8 @@ function CardOne({username, btnText, image}) {
     console.log(username,btnText,image);
   return (
     <div className="ml-10 mt-10 relative h-[400px] w-[300px] rounded-md">
-      <img
-        src={image}
-      />
+    <img src={`.${image}`} className="z-0 h-full w-full rounded-md object-cover" />
+
       <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
       <div className="absolute bottom-4 left-4 text-left">
         <h1 className="text-lg font-semibold text-white">{username}</h1>
